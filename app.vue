a6dff96e95b3199c1bef898184225259f9ad2ea3
JustStone <kripto.doctor@gmail.com>
2024-03-03 21:20:35 +1000

    first commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+
+import AppAlert from './components/AppAlert.vue';
+
+
+<style>
+@import url(~/assets/css/first.css);
+</style>
+
+<script>
+console.log("\nHello world!\n")
+</script>
+
+<template>
+  <div>
+
+    <h1>Hello world!!!!!!!!</h1>
+
+    <AppAlert class="alert">
+      This is an auto-imported component.
+    </AppAlert>
+
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/google-fonts'],
+
+  googleFonts: {
+    families: {
+      Montserrat: true,
+      'Open Sans': true
+    }
+  }
+})
